b37b2dfb5d60c4fb9a43c63ede358c0dd2c67d7f
Aaron723 <w745207699@gmail.com>
2019-10-17 16:45:44 -0400

    add user db and register, login

--- Project1/utils.py
+++ Project1/utils.py
@@ -6,19 +6,74 @@ from time import time
 class connect_mysql():
     def __init__(self, host="localhost", user="myuser", password=None, db="db"):
         self.db = pymysql.connect(host, user, password, db)
-        self.cursor = self.db.cursor()
+        self.cursor = self.db.cursor(cursor=pymysql.cursors.DictCursor)
 
     def run_query(self, query_statement):
         start_time = int(round(time() * 1000))
-        self.cursor.execute(query_statement)
-        result = self.cursor.fetchall()
-        query_time = str(int(round(time() * 1000)) - start_time) + " ms"
-        if len(result) > 100000:
-            result = result[:9999]
-            query_time += '\nThe result is too larger to transmit, so we limit the size to return'
-        return result, query_time
+        try:
+            self.cursor.execute(query_statement)
+            result = self.cursor.fetchall()
+            query_time = str(int(round(time() * 1000)) - start_time) + " ms"
+            if len(result) > 100000:
+                result = result[:9999]
+                query_time += '\nThe result is too larger to transmit, so we limit the size to return'
+            return result, query_time
+        except:
+            self.db.rollback()
+            query_time = str(int(round(time() * 1000)) - start_time) + " ms"
+            return "the statement hasn't executed properly!", query_time
+
+    def register_user(self, user_name, password, email_address):
+        user_id, password = hash(user_name), hash(password)
+        # detect if there already has the same user_id
+        query = 'SELECT COUNT(*) FROM user_info WHERE {}={}'
+        # query_id = query.format('user_id', user_id)
+        # if the userd name exist, insert, else ignore it
+        sql = 'INSERT IGNORE INTO user_info (user_id, password, email_address) VALUES ({}, {}, "{}");'.format(
+            str(user_id),
+            str(password),
+            email_address)
+        try:
+            self.cursor.execute(query.format('user_id', str(user_id)))
+            query_id = self.cursor.fetchall()[0]['COUNT(*)']
+            self.cursor.execute(query.format('email_address', '"' + email_address + '"'))
+            query_email = self.cursor.fetchall()[0]['COUNT(*)']
+            # ('email_address', email_address)
+            if query_email > 0 and query_id > 0:
+                return 'user name and email address has been used'
+            elif query_id < 1 and query_email > 0:
+                return 'email address has been used'
+            elif query_id > 0:
+                return 'user name has been used'
+            else:
+                self.cursor.execute(sql)
+                self.db.commit()
+                return 'register success'
+        except:
+            self.db.rollback()
+            return "the statement hasn't executed properly!"
+
+    def login(self, user_name, password, email_address):
+        user_id, password = hash(user_name) if user_name is not None else user_name, hash(password)
+        # .format(user_id if user_id is not None else email_address, password)
+        if user_id is None:
+            query = 'SELECT {}, password FROM user_info WHERE {}="{}"'.format('email_address', "email_address",
+                                                                              email_address)
+        else:
+            query = 'SELECT {}, password FROM user_info WHERE {}={}'.format('user_id', 'user_id', user_id)
+        try:
+            self.cursor.execute(query)
+            content = self.cursor.fetchall()[0]['password']
+        except:
+            self.db.rollback()
+            return "wrong user name or wrong email address"
+        if content == password:
+            return 'login sucessful'
+        else:
+            return 'wrong password'
 
     def disconnect(self):
+        self.cursor.close()
         self.db.close()
 
 
@@ -29,14 +84,19 @@ class connect_redshift():
 
     def run_query(self, query_statement):
         start_time = int(round(time() * 1000))
-        self.cur.execute(query_statement)
-        result = self.cur.fetchall()
-        query_time = str(int(round(time() * 1000)) - start_time) + " ms"
-        if len(result) > 100000:
-            result = result[:9999]
-            query_time += '\nThe result is too larger to transmit, so we limit the size to return'
+        try:
+            self.cur.execute(query_statement)
+            result = self.cur.fetchall()
+            query_time = str(int(round(time() * 1000)) - start_time) + " ms"
+            if len(result) > 100000:
+                result = result[:9999]
+                query_time += '\nThe result is too larger to transmit, so we limit the size to return'
 
-        return result, query_time
+            return result, query_time
+        except:
+            self.con.rollback()
+            query_time = str(int(round(time() * 1000)) - start_time) + " ms"
+            return "the statement hasn't executed properly!", query_time
 
     def disconnect(self):
         self.cur.close()
--- Project1/views.py
+++ Project1/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from utils import connect_mysql, connect_redshift
 
 app = Flask(__name__)
@@ -30,3 +30,29 @@ def query_redshift():
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    user_info = request.get_json()
+    user_name = user_info.get('user_name', None)
+    password = user_info.get('password', None)
+    email_address = user_info.get('email_address', None)
+    connection = connect_mysql(host='database-1.cqggblhrtnvj.us-east-1.rds.amazonaws.com', user='admin',
+                               password='12345678', db='user')
+    result = connection.register_user(user_name=user_name, password=password, email_address=email_address)
+    connection.disconnect()
+    return jsonify({'result': result})
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    user_info = request.get_json()
+    user_name, password, email_address = user_info.get('user_name', None), user_info.get('password',
+                                                                                         None), user_info.get(
+        'email_address', None)
+    connection = connect_mysql(host='database-1.cqggblhrtnvj.us-east-1.rds.amazonaws.com', user='admin',
+                               password='12345678', db='user')
+    result = connection.login(user_name=user_name, password=password, email_address=email_address)
+    connection.disconnect()
+    return jsonify({'result': result})
