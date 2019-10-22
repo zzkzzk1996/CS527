852c39ddda8d055c315b48b7c662a59140bf3a07
Mo Shi <mo.shi@rutgers.edu>
2019-10-22 10:37:01 -0400

    Add files via upload

---
+++ Project1/views.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+from utils import connect_mysql, connect_redshift
+
+app = Flask(__name__)
+
+
+@app.route('/mysql', methods=['GET'])
+def query_mysql():
+    query = request.args.get('query', 'show tables;')
+    connection = connect_mysql(host='cs527mysql2.chmrmo5grph7.us-east-1.rds.amazonaws.com', user='Maaaartian',
+                               password='mysqls250+38qqcn', db='test')
+    content, query_time = connection.run_query(query)
+    result = {'result': content, 'query_time': query_time}
+    connection.disconnect()
+    return result
+
+
+@app.route('/redshift', methods=['GET'])
+def query_redshift():
+    query = request.args.get('query', 'show tables;')
+    connection = connect_redshift(host='cs527redshift.clyua6krsyew.us-east-1.redshift.amazonaws.com', user='group3',
+                                  password='Redshift123',
+                                  database='dev')
+    content, query_time = connection.run_query(query)
+    result = {'result': content, 'query_time': query_time}
+    connection.disconnect()
+    return result
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
