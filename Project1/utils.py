316da71f0e4c9e419d79d928c90f35bf60b5d065
zzkzzk1996 <zekunzhang.1996@gmail.com>
2019-10-22 14:14:23 -0400

    Bug fixed

--- Project1/utils.py
+++ Project1/utils.py
@@ -6,73 +6,10 @@ from time import time
 class connect_mysql():
     def __init__(self, host="localhost", user="myuser", password=None, db="db"):
         self.db = pymysql.connect(host, user, password, db)
-        self.cursor = self.db.cursor(cursor=pymysql.cursors.DictCursor)
+        self.cursor = self.db.cursor()
 
     def run_query(self, query_statement):
         start_time = int(round(time() * 1000))
-<<<<<<< Updated upstream
-        try:
-            self.cursor.execute(query_statement)
-            result = self.cursor.fetchall()
-            query_time = str(int(round(time() * 1000)) - start_time) + " ms"
-            if len(result) > 100000:
-                result = result[:9999]
-                query_time += '\nThe result is too larger to transmit, so we limit the size to return'
-            return result, query_time
-        except:
-            self.db.rollback()
-            query_time = str(int(round(time() * 1000)) - start_time) + " ms"
-            return "the statement hasn't executed properly!", query_time
-
-    def register_user(self, user_name, password, email_address):
-        user_id, password = hash(user_name), hash(password)
-        # detect if there already has the same user_id
-        query = 'SELECT COUNT(*) FROM user_info WHERE {}={}'
-        # query_id = query.format('user_id', user_id)
-        # if the userd name exist, insert, else ignore it
-        sql = 'INSERT IGNORE INTO user_info (user_id, password, email_address) VALUES ({}, {}, "{}");'.format(
-            str(user_id),
-            str(password),
-            email_address)
-        try:
-            self.cursor.execute(query.format('user_id', str(user_id)))
-            query_id = self.cursor.fetchall()[0]['COUNT(*)']
-            self.cursor.execute(query.format('email_address', '"' + email_address + '"'))
-            query_email = self.cursor.fetchall()[0]['COUNT(*)']
-            # ('email_address', email_address)
-            if query_email > 0 and query_id > 0:
-                return 'user name and email address has been used'
-            elif query_id < 1 and query_email > 0:
-                return 'email address has been used'
-            elif query_id > 0:
-                return 'user name has been used'
-            else:
-                self.cursor.execute(sql)
-                self.db.commit()
-                return 'register success'
-        except:
-            self.db.rollback()
-            return "the statement hasn't executed properly!"
-
-    def login(self, user_name, password, email_address):
-        user_id, password = hash(user_name) if user_name is not None else user_name, hash(password)
-        # .format(user_id if user_id is not None else email_address, password)
-        if user_id is None:
-            query = 'SELECT {}, password FROM user_info WHERE {}="{}"'.format('email_address', "email_address",
-                                                                              email_address)
-        else:
-            query = 'SELECT {}, password FROM user_info WHERE {}={}'.format('user_id', 'user_id', user_id)
-        try:
-            self.cursor.execute(query)
-            content = self.cursor.fetchall()[0]['password']
-        except:
-            self.db.rollback()
-            return "wrong user name or wrong email address"
-        if content == password:
-            return 'login sucessful'
-        else:
-            return 'wrong password'
-=======
         self.cursor.execute(query_statement)
         col_info = self.cursor.description
         result = self.cursor.fetchall()
@@ -84,16 +21,8 @@ class connect_mysql():
         for i in range(len(col_info)):
             col_name.append(col_info[i][0])
         return col_name, result, query_time
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
     def disconnect(self):
-        self.cursor.close()
         self.db.close()
 
 
@@ -104,21 +33,6 @@ class connect_redshift():
 
     def run_query(self, query_statement):
         start_time = int(round(time() * 1000))
-<<<<<<< Updated upstream
-        try:
-            self.cur.execute(query_statement)
-            result = self.cur.fetchall()
-            query_time = str(int(round(time() * 1000)) - start_time) + " ms"
-            if len(result) > 100000:
-                result = result[:9999]
-                query_time += '\nThe result is too larger to transmit, so we limit the size to return'
-
-            return result, query_time
-        except:
-            self.con.rollback()
-            query_time = str(int(round(time() * 1000)) - start_time) + " ms"
-            return "the statement hasn't executed properly!", query_time
-=======
         self.cur.execute(query_statement)
         col_info = self.cur.description
         result = self.cur.fetchall()
@@ -130,13 +44,6 @@ class connect_redshift():
         for i in range(len(col_info)):
             col_name.append(col_info[i][0])
         return col_name, result, query_time
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
     def disconnect(self):
         self.cur.close()
--- Project1/views.py
+++ Project1/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify, render_template, redirect, make_response
 from utils import connect_mysql, connect_redshift
 
 app = Flask(__name__)
@@ -7,25 +7,10 @@ app = Flask(__name__)
 @app.route('/mysql', methods=['GET'])
 def query_mysql():
     query = request.args.get('query', 'show tables;')
-    connection = connect_mysql(host='cs527mysql2.chmrmo5grph7.us-east-1.rds.amazonaws.com', user='Maaaartian',
-                               password='mysqls250+38qqcn', db='test')
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+    connection = connect_mysql(host='database-3.c8ainggp8y19.us-east-1.rds.amazonaws.com', user='admin',
+                               password='12345678', db='cs527')
     content, query_time = connection.run_query(query)
     result = {'result': content, 'query_time': query_time}
-=======
-    col_name, content, query_time = connection.run_query(query)
-    result = {'col_name': col_name, 'result': content, 'query_time': query_time}
->>>>>>> Stashed changes
-=======
-    col_name, content, query_time = connection.run_query(query)
-    result = {'col_name': col_name, 'result': content, 'query_time': query_time}
->>>>>>> Stashed changes
-=======
-    col_name, content, query_time = connection.run_query(query)
-    result = {'col_name': col_name, 'result': content, 'query_time': query_time}
->>>>>>> Stashed changes
     connection.disconnect()
     return result
 
@@ -33,26 +18,10 @@ def query_mysql():
 @app.route('/redshift', methods=['GET'])
 def query_redshift():
     query = request.args.get('query', 'show tables;')
-    connection = connect_redshift(host='cs527redshift.clyua6krsyew.us-east-1.redshift.amazonaws.com', user='group3',
-                                  password='Redshift123',
+    connection = connect_redshift(host='redshift-cluster-1.cxyojdkh3h9f.us-east-1.redshift.amazonaws.com',
                                   database='dev')
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     content, query_time = connection.run_query(query)
     result = {'result': content, 'query_time': query_time}
-=======
-    col_name, content, query_time = connection.run_query(query)
-    result = {'col_name': col_name, 'result': content, 'query_time': query_time}
->>>>>>> Stashed changes
-=======
-    col_name, content, query_time = connection.run_query(query)
-    result = {'col_name': col_name, 'result': content, 'query_time': query_time}
->>>>>>> Stashed changes
-=======
-    col_name, content, query_time = connection.run_query(query)
-    result = {'col_name': col_name, 'result': content, 'query_time': query_time}
->>>>>>> Stashed changes
     connection.disconnect()
     return result
 
